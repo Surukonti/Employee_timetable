5fa45d9139762a742a62be1f00ec6e769c0dd65f
Rammohan Surukonti <Rammohan.surukonti@itsd-consulting.de>
2018-09-11 16:07:30 +0200

    cross origin and configuration

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,10 +1,18 @@
 package app;
 
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -18,4 +26,13 @@ public class Application {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			//@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api").allowedOrigins("\"http://localhost:4200, credentials: true\"");
+			}
+		};
+}}
+
--- src/main/java/app/springbootdemo/controller/EmployeeController.java
+++ src/main/java/app/springbootdemo/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200, credentials: true")
 @RestController
 @RequestMapping(value="/api")
 public class EmployeeController {
