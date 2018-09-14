9f9d34b4d2d0973c16e2b131366831fe84cc3701
Rammohan Surukonti <Rammohan.surukonti@itsd-consulting.de>
2018-09-14 15:53:12 +0200

    CorsFilter

---
+++ src/main/java/app/springbootdemo/login/security/CorsFilter.java
@@ -0,0 +1,38 @@
+package app.springbootdemo.login.security;
+
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * @author Alex Tymzjas
+ *
+ */
+public class CorsFilter extends OncePerRequestFilter {
+    private String allowedOrigins;
+
+    public CorsFilter(String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+        response.setHeader("Access-Control-Max-Age", "3600");
+
+        filterChain.doFilter(request, response);
+    }
+
+}
--- src/main/java/app/springbootdemo/login/security/WebSecurity.java
+++ src/main/java/app/springbootdemo/login/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package app.springbootdemo.login.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,14 +14,17 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import static app.springbootdemo.login.security.SecurityConstants.SIGN_UP_URL;
+
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter { //this extenstion class to customize security frameworks to our needs
     private UserDetailsService userDetailsService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public WebSecurity(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception { //where we define which resources r publc n secured.
         http.csrf().disable().authorizeRequests()
@@ -30,15 +34,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter { //this extenstio
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()));
     }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder); //to load user specifc data. used to set the encrpt method used by our application.
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-//        return (CorsConfigurationSource) source;
-//    }
-}
+
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+    final FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+    registrationBean.setFilter(new CorsFilter("*"));
+    registrationBean.addUrlPatterns("/*");
+    return registrationBean;
+    }}
+
